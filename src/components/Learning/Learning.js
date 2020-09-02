9f29471c88eb03efee103b9f7168bf39430a4e2b
Josh Struve <jstruve@gmail.com>
2020-09-02 10:46:13 -0700

    learning components

---
+++ src/components/Learning/Learning.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { MdCheckCircle, MdCancel } from 'react-icons/md'
+
+class Learning extends Component {
+    render() {
+      return (
+        <section>
+          implement and style me
+        </section>
+      );
+    }
+  }
+  
+  export default Learning
