76aa7151b7910e09fbc30788f7c9394000089f55
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-07 10:36:00 -0400

    deployed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-	API_ENDPOINT: 'http://localhost:8000/api',
+	API_ENDPOINT: 'https://iro-api.herokuapp.com/api',
+	// API_ENDPOINT: 'http://localhost:8000/api'
 	TOKEN_KEY: 'spaced-repetition-jwt-secret',
 }
